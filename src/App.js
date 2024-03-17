223cd34cdff1cc39a6cef2f2d0268b67eeaf0fdb
iamharsha24 <harsha7024@gmail.com>
2024-03-17 11:23:02 +0530

    updated perfumey

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
+import Products from "./components/Products";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Search />
+      <Products />
+      <About />
+      <Footer />
+    </div>
+  );
+}
+export default App;
